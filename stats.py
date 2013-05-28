23c117be9aa96c18d3e9518fb120dfcaaf933540
Jonathan Acosta <jonathan@poifox.com>
2013-05-28 12:55:18 -0600

    Changed formating of returned object to a tuple of dictionaries, now results can be iterated in PHP

--- stats.py
+++ stats.py
@@ -13,7 +13,8 @@ import os
 
 context = bpy.context
 output = {}
-stats = {}
+
+
 
 def mode_set_object():
 
@@ -49,6 +50,8 @@ def is_valid_path(filepath, basepath):
     print("realpath", realpath)
     return realpath.startswith(basepath) and os.path.exists(realpath) and os.path.isfile(realpath)
 
+# holds the stats array
+scenes = []
 
 for scene in bpy.data.scenes:
 
@@ -58,32 +61,41 @@ for scene in bpy.data.scenes:
 
     mode_set_object()
 
-
     verts = sum(len(ob.data.vertices) for ob in scene.objects if ob.type == 'MESH')
     polygons = sum(len(ob.data.polygons) for ob in scene.objects if ob.type == 'MESH')
     render_engine = scene.render.engine
     objects_mesh = len([ob for ob in scene.objects if ob.type == 'MESH'])
 
-    builtin_stats = scene.statistics() # includes modifiers!
+    builtin_stats = scene.statistics()  # includes modifiers!
 
     for segment in builtin_stats.split(" | "):
         segment_split = segment.split(":")
         if segment_split[0] == "Verts":
-           verts_modified = int(segment_split[1])
+            verts_modified = int(segment_split[1])
         elif segment_split[0] == "Faces":
-           polygons_modified = int(segment_split[1])
+            polygons_modified = int(segment_split[1])
         elif segment_split[0] == "Tris":
-           triangles = int(segment_split[1])
+            triangles = int(segment_split[1])
 
-    stats[scene.name] = {
-        'verts': verts,
-        'verts_modified': verts_modified,
-        'polygons': polygons,
-        'polygons_modified': polygons_modified,
-        'triangles': triangles,
-        'render_engine': render_engine,
-        'objects_mesh': objects_mesh,
-    }
+    # stats[scene.name] = {
+    #     'verts': verts,
+    #     'verts_modified': verts_modified,
+    #     'polygons': polygons,
+    #     'polygons_modified': polygons_modified,
+    #     'triangles': triangles,
+    #     'render_engine': render_engine,
+    #     'objects_mesh': objects_mesh,
+    # }
+    this_scene = {
+        'name':                 scene.name,
+        'verts':                verts,
+        'verts_modified':       verts_modified,
+        'polygons':             polygons,
+        'polygons_modified':    polygons_modified,
+        'triangles':            triangles,
+        'render_engine':        render_engine,
+        'objects_mesh':         objects_mesh}
+    scenes.append(this_scene)
 
 
 # Get script parameters
@@ -94,13 +106,12 @@ try:
     # all list items after the last occurence of "--"
     #idx = len(sys.argv) - list(reversed(sys.argv)).index("--")
 
-    idx = len(sys.argv)  - 1
+    idx = len(sys.argv) - 1
     while idx >= 0:
         if sys.argv[idx] == "--":
             idx += 1
             break
         idx -= 1
-
 
     # additional sanity check, might decrease lower boundary value
     if idx < 6 or idx >= len(sys.argv):
@@ -108,20 +119,23 @@ try:
 except ValueError:
     params = []
 else:
-    params = sys.argv[idx:] # if .index -> idx+1
+    params = sys.argv[idx:]  # if .index -> idx+1
 print("Script params:", params)
 
 
-output['stats'] = stats
+output['scenes'] = scenes
 
 bad_images = []
 
 
 for image in bpy.data.images:
-    if ((image.users > 0 and image.packed_file is None and image.filepath and not is_valid_path(image.filepath, base_path)) \
-       or image.library is not None) \
-       and image.name not in bad_images:
-
+    img_ud = image.users > 0
+    img_pk = image.packed_file is None
+    img_pt = image.filepath
+    img_vd = is_valid_path(image.filepath, base_path)
+    img_lib = image.library is not None
+    img_is_fine = (img_ud and img_pk and img_pt and not img_vd) or img_lib
+    if img_is_fine and image.name not in bad_images:
         bad_images.append(image.name)
 
 output['bad_images'] = bad_images
